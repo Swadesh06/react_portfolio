74a9a0a3085c4a217ae74399b00b4e44e1fa04d5
Swadesh06 <swadesh_s@ece.iitr.ac.in>
2024-08-17 16:56:20 +0530

    second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,15 +22,13 @@ const Hero = () => {
                 <div className="flex flex-col items-center lg:items-start">
                     <motion.h1 
                     variants={container(0)}
-                    initial={{x:-100, opacity: 0}}
-                    intial="hidden"
+                    initial="hidden"
                     animate="visible"
                     className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Swadesh Swain</motion.h1>
 
                     <motion.span   
                     variants={container(0.5)}
-                    initial={{x:-100, opacity: 0}}
-                    intial="hidden"
+                    initial="hidden"
                     animate="visible"
                     className="bg-gradient-to-r from-pink-300 via-slate-50 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
                     >
@@ -38,8 +36,7 @@ const Hero = () => {
                     </motion.span>
 
                     <motion.p
-                    variants={container(1)}
-                    initial={{x:-100, opacity: 0}}                   
+                    variants={container(1)}               
                     intial="hidden"
                     animate="visible"                    
                     className="my-2 max-w-xl py-6 font-light tracking-tigher">
