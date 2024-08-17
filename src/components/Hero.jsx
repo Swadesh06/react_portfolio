47f92d1c0e8365b4375efbda65a2d7bc5ec3ddd5
Swadesh Swain <swadesh_s@ece.iitr.ac.in>
2024-08-17 23:53:16 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import{ HERO_CONTENT } from "../constants";
-import profilePic from "src/assets/kevinRushProfile.png";
+import profilePic from "../assets/kevinRushProfile.png";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
